5fb808ec71ada92446266200e218afd7d25e81a8
andrey <volosgoto@yahoo.com>
2019-03-15 22:08:39 +0200

    set bootstrap redux react-redux react-router axious

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
+
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Home page</h1>
+        <input type="button" value="" className="btn btn-danger" value="Test" />
       </div>
     );
   }
