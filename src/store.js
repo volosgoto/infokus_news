41e19597d1d83c08845da808561f9e01b34a42f6
andrey <volosgoto@yahoo.com>
2019-03-15 23:16:43 +0200

    set app store

--- src/store.js
+++ src/store.js
@@ -1,1 +1,18 @@
 import { createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // comment
+  )
+);
+
+export default store;
