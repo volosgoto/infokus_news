a0c2794f0e9955991fad0e998216e4cd17e3b88f
andrey <volosgoto@yahoo.com>
2019-03-15 22:55:40 +0200

    refactor export in Qoute and Qoutes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Quote extends Component {
+class Quote extends Component {
   render() {
     return (
       <div>
@@ -9,3 +9,5 @@ export default class Quote extends Component {
     )
   }
 }
+
+export default Quote;
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Quotes extends Component {
+class Quotes extends Component {
   render() {
     return (
       <div>
@@ -9,3 +9,4 @@ export default class Quotes extends Component {
     )
   }
 }
+export default Quotes
