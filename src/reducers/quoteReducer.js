886ed34ae749e3bfa9ec4a60f631fb44c7c39e13
andrey <volosgoto@yahoo.com>
2019-03-15 23:22:54 +0200

    init quoteReducer

---
+++ src/reducers/quoteReducer.js
@@ -0,0 +1,17 @@
+import { GET_CATEGORY, GET_QUOTE } from '../actions/types';
+
+const initialState = {
+  categories: [],
+  quote: {}
+};
+
+export default function (state = initialState, action) {
+  switch (key) {
+    case value:
+
+      break;
+
+    default:
+      break;
+  }
+}
